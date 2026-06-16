48a440b4691460e231948b73e9b1cfe71120eb44
NICOLLY FERNANDA AURELIANO PEREIRA <nicolly_f_pereira@estudante.sesisenai.org.br>
2026-06-15 22:09:11 -0300

    css

--- cadastroCliente.php
+++ cadastroCliente.php
@@ -7,7 +7,7 @@ include("php/funcoes.php")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_clientes.css">
     <title>Cadastro de Clientes</title>
 </head>
 <body>
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>LOGIN</title>
 
-  <link rel="stylesheet" href="estilo.css">
+  <link rel="stylesheet" href="estilo_login.css">
 </head>
 <body>
 
